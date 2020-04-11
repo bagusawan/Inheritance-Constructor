8328e5168d228eaa14d881a8b2d227a49d894cef
bagusawan <61877746+bagusawan@users.noreply.github.com>
2020-04-11 18:41:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("hehe",18);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("richman",30,696969,"github","Dosen@teacher.amikom.ac.id");
+            teacher.GetNameAndAge();
+            
+            Student student = new Student("awan",18,2686,"bagus.17@student.amikom.ac.id");
+            student.GetNameAndAge();
+        }
+    }
+}
